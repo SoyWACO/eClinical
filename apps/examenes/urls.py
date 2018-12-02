e6c260a21b50f5d1c84d4e332b4d1bb9737cd864
William <soywaco@gmail.com>
2018-12-01 20:34:33 -0600

    Arreglo en error de urls de examenes.

--- apps/examenes/urls.py
+++ apps/examenes/urls.py
@@ -20,18 +20,18 @@ urlpatterns = [
     path('examen_sangre/', ExamenSangreList.as_view(), name='examen_sangre_list'),
 	path('examen_sangre/add/', ExamenSangreCreate.as_view(), name='examen_sangre_create'),
 	path('examen_sangre/<pk>/change/', ExamenSangreUpdate.as_view(), name='examen_sangre_update'),
-	path('/<pk>/delete/',ExamenSangreDelete.as_view(), name='examen_sangre_delete'),
+	path('examen_sangre/<pk>/delete/',ExamenSangreDelete.as_view(), name='examen_sangre_delete'),
 
 	#examen de orina
     path('examen_orina/', ExamenOrinaList.as_view(), name='examen_orina_list'),
 	path('examen_orina/add/', ExamenOrinaCreate.as_view(), name='examen_orina_create'),
 	path('examen_orina/<pk>/change/', ExamenOrinaUpdate.as_view(), name='examen_orina_update'),
-	path('/<pk>/delete/',ExamenOrinaDelete.as_view(), name='examen_orina_delete'),
+	path('examen_orina/<pk>/delete/',ExamenOrinaDelete.as_view(), name='examen_orina_delete'),
 
    #examen de heces
     path('examen_heces/', ExamenHecesList.as_view(), name='examen_heces_list'),
 	path('examen_heces/add/', ExamenHecesCreate.as_view(), name='examen_heces_create'),
 	path('examen_heces/<pk>/change/', ExamenHecesUpdate.as_view(), name='examen_heces_update'),
-	path('/<pk>/delete/',ExamenHecesDelete.as_view(), name='examen_heces_delete'),
+	path('examen_heces/<pk>/delete/',ExamenHecesDelete.as_view(), name='examen_heces_delete'),
 
 ]
